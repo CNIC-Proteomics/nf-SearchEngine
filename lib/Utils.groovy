3f8abda8c10cd3d7ae94e7c9af7f5afb997efb29
jmrodriguezc <jmrodriguezc1977@gmail.com>
2024-02-26 14:53:45 +0100

    developing

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -116,21 +116,21 @@ class Utils {
         println("PARAM_FILE ${ifile}")
 
         // parse the MSF file
-        def ini_data = parseMsfFile(ifile)
-        // // update the given attributes
-        // try {
-        //     replaces.each { replace ->
-        //         if ( ini_data.containsKey(replace.key) ) {
-        //             ini_data[replace.key] = replace.value
-        //         }
-        //         else {
-        //             throw new Exception("Key '$replace.key' is not in the parameter file.")
-        //         }
-        //     }
-        // } catch(Exception ex) {
-        //     println("ERROR:${new Object(){}.getClass().getEnclosingMethod().getName()}:$ex")
-        //     System.exit(1)
-        // }
+        def ini_data = parseMsfFile(ifile.toString())
+        // update the given attributes
+        try {
+            replaces.each { replace ->
+                if ( ini_data.containsKey(replace.key) ) {
+                    ini_data[replace.key] = replace.value
+                }
+                else {
+                    throw new Exception("Key '$replace.key' is not in the parameter file.")
+                }
+            }
+        } catch(Exception ex) {
+            println("ERROR:${new Object(){}.getClass().getEnclosingMethod().getName()}:$ex")
+            System.exit(1)
+        }
 
         return ini_data
     }
